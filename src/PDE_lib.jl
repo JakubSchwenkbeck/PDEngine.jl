c488a252b66866f68a00577808a4b215712ac145
Jakub <Jakub@schwenkbeck.de>
2024-08-20 12:47:05 +0200

    Added info about new solvers

--- src/PDE_lib.jl
+++ src/PDE_lib.jl
@@ -10,20 +10,25 @@ include("navier_stokes_eq.jl")
 include("poisson_eq.jl")
 
 # Export functions for external use
-export heat_eq_1d_fdm,heat_eq_1d_fem,
- wave_eq_1d_fdm,wave_eq_1d_fem
-  navier_stokes_2d, 
-  poisson_2d
+export heat_eq_1d_fdm,  heat_eq_1d_fem,
+
+       wave_eq_1d_fdm,  wave_eq_1d_fem,
+
+       navier_stokes_2d, 
+
+       poisson_2d
 
 # Wrapper functions or additional setup 
 # initialize some parameters or configurations
 
-# Example function - basic information about the package
+#  basic information about the package
 function info()
     @info "PDEngine.jl: A Julia library for solving partial differential equations (PDEs)."
     @info "Available solvers:"
-    @info "- heat_eq_1d: Solves the 1D heat equation."
-    @info "- wave_eq_1d: Solves the 1D wave equation."
+    @info "- heat_eq_1d_fdm: Solves the 1D heat equation with the finite difference method"
+    @info "- heat_eq_1d_fem: Solves the 1D heat equation with the finite element method"
+    @info "- wave_eq_1d_fdm: Solves the 1D wave equation with the finite difference method"
+    @info "- wave_eq_1d_fem: Solves the 1D wave equation with the finite element method"
     @info "- navier_stokes_2d: Solves the 2D Navier-Stokes equations."
     @info "- poisson_2d: Solves the 2D Poisson's equation."
 end
