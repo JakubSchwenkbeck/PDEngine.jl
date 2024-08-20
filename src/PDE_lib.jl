49a7cf4fdbabb5348aa4f4952d5d559d8a08ef87
Jakub <Jakub@schwenkbeck.de>
2024-08-20 11:48:17 +0200

    added manifest and altered main module

--- src/PDE_lib.jl
+++ src/PDE_lib.jl
@@ -6,11 +6,11 @@ using Printf  # For formatted printing
 # Include PDE solver files
 include("heat_eq.jl")
 include("wave_eq.jl")
-include("navier_stokes.jl")
-include("poisson.jl")
+include("navier_stokes_eq.jl")
+include("poisson_eq.jl")
 
 # Export functions for external use
-export heat_eq_1d, wave_eq_1d, navier_stokes_2d, poisson_2d
+export heat_eq_1d_fdm,heat_eq_1d_fem, wave_eq_1d, navier_stokes_2d, poisson_2d
 
 # Wrapper functions or additional setup 
 # initialize some parameters or configurations
