1f6cd5182434ecca67cae367144da343d1c95003
Jakub <Jakub@schwenkbeck.de>
2024-08-20 13:13:04 +0200

    minor changes to wave eq

--- src/wave_eq.jl
+++ src/wave_eq.jl
@@ -33,7 +33,6 @@ function wave_eq_1d_fdm(N, c, T, Δx, Δt)
     return u
 end
 
-
 """
 Solve the 1D wave equation using the finite element method (FEM).
 The wave equation is given by: ∂²u/∂t² = c² ∇²u
@@ -55,48 +54,49 @@ function wave_eq_1d_fem(N, c, T, Δx, Δt)
     
     # Create a grid of spatial points
     nodes = 0:Δx:1
-    num_nodes = length(nodes)
 
     # Initialize displacement and velocity fields
-    u = zeros(num_nodes)     # Displacement at current time step
-    u_prev = similar(u)      # Displacement at previous time step
-    u_next = similar(u)      # Displacement at next time step
+    u = zeros(N+1)     # Displacement
+    u_prev = similar(u) # Displacement at previous time step
+    u_next = similar(u) # Displacement at next time step
 
-    # Initialize global matrices
-    M = zeros(num_nodes, num_nodes) # Mass matrix
-    K = zeros(num_nodes, num_nodes) # Stiffness matrix
+    # Initialize stiffness matrix (K) and mass matrix (M)
+    K = zeros(N+1, N+1)
+    M = zeros(N+1, N+1)
 
-    # Assemble global matrices
-    for i in 1:(N)
+    # Assemble stiffness matrix and mass matrix
+    for i in 1:N
+        # Local matrices for element i
         x1 = nodes[i]
         x2 = nodes[i+1]
         L = x2 - x1
-        m_local = (L / 6) * [2 1; 1 2]
-        k_local = (1 / L) * [1 -1; -1 1]
-        M[i:i+1, i:i+1] += m_local
+        k_local = (c^2 / L) * [1 -1; -1 1]
+        m_local = L / 6 * [2 1; 1 2]
+
+        # Assemble into global stiffness and mass matrices
         K[i:i+1, i:i+1] += k_local
+        M[i:i+1, i:i+1] += m_local
     end
 
     # Apply boundary conditions (Dirichlet: u = 0 at the boundaries)
-    M[1, :] .= 0
-    M[1, 1] = 1
     K[1, :] .= 0
     K[1, 1] = 1
-
-    M[end, :] .= 0
-    M[end, end] = 1
+    M[1, :] .= 0
+    M[1, 1] = 1
     K[end, :] .= 0
     K[end, end] = 1
+    M[end, :] .= 0
+    M[end, end] = 1
 
     # Time-stepping loop
     for _ in 1:num_steps
-        # Update the load vector based on the previous solution
-        F = M * (u - u_prev) / Δt^2 + K * u
+        # Compute the right-hand side vector
+        f = M * u_next - K * u_prev
         
         # Solve for the new displacement field
-        u_next = M \ F
+        u_next = K \ f
         
-        # Update displacement fields for the next time step
+        # Update displacement fields
         u_prev, u, u_next = u, u_next, u_prev
     end
 
